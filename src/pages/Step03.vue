40561784974e8cc7dd1cf7b69bdfa2ff7d84c4d7
xingzi <xingzi@yuansfer.com>
2019-08-14 16:34:09 +0800

    修改字段，改为复选支付宝，微信支付，银联支付

--- src/pages/Step03.vue
+++ src/pages/Step03.vue
@@ -2,23 +2,58 @@
  <div>
    <p class="my-step-title">Step 3 of 10 </p>
    <el-progress :text-inside="true" :stroke-width="26" :percentage="30"></el-progress>
-   <p style="text-align: center;margin-bottom: 30px">Appendix 2</p>
+   <p style="margin-bottom: 30px">Service and Fee Schedule</p>
    <p style="margin: 0;padding: 0">The Service Fee shall be applied at the rate set forth in the table below:</p>
-   <p style="">Applicable rate for the Service Fee<span class="must-label">*</span></p>
-   <el-input v-model="serviceFee" style="background: #E6E6E6;width: 300px" placeholder="2.9% of the Aggregate Payment Amount"></el-input>
+   <p style="">Offline Processing Rate<span class="must-label">*</span></p>
+   <el-row :gutter="20">
+     <el-checkbox-group v-model="checkList">
+       <el-col :span="8" class="offline-rate">
+         <el-checkbox label="alipayOffline">Alipay offline</el-checkbox>
+         <el-input size="mini" class="inp" placeholder="2.3%" :disabled="true"></el-input>
+       </el-col>
+       <el-col :span="8" class="offline-rate"><div>
+         <el-checkbox label="wechatpayOffline">WeChatPay Offline</el-checkbox>
+         <el-input size="mini" class="inp" placeholder="1.8%" :disabled="true"></el-input>
+       </div></el-col>
+       <el-col :span="8" class="offline-rate"><div>
+         <el-checkbox label="unionpayQRCode">UnionPay QR Code</el-checkbox>
+         <el-input size="mini" class="inp" placeholder="2.3%" :disabled="true"></el-input>
+       </div></el-col>
+     </el-checkbox-group>
+   </el-row>
    <p>Do you need Online API (For Ecommerce Only)<span class="must-label">*</span></p>
    <el-radio-group v-model="onlineAPI" class="radio-group">
      <div class="my-radio"><el-radio label="yes">Yes</el-radio></div>
      <div class="my-radio"><el-radio label="no">No</el-radio></div>
    </el-radio-group>
    <div v-show="needOnlineAPI">
-     <p>Online Applicable rate for Service Fee</p>
-     <el-input v-model="onlineServiceFee" style="background: #E6E6E6;width: 300px" placeholder="2.9% of the Aggregate Payment Amount"></el-input>
+     <p>Online Processing Rate</p>
+     <el-row :gutter="20">
+       <el-checkbox-group v-model="checkList">
+         <el-col :span="8" class="offline-rate">
+           <el-checkbox label="alipayOnline">Alipay Online</el-checkbox>
+           <el-input size="mini" class="inp" placeholder="2.3%" :disabled="true"></el-input>
+         </el-col>
+         <el-col :span="8" class="offline-rate"><div>
+           <el-checkbox label="wechatpayOffline">WeChatPay Online</el-checkbox>
+           <el-input size="mini" class="inp" placeholder="1.8%" :disabled="true"></el-input>
+         </div></el-col>
+         <el-col :span="8" class="offline-rate"><div>
+           <el-checkbox label="unionpayQRCode">UnionPay Online</el-checkbox>
+           <el-input size="mini" class="inp" placeholder="2.3%" :disabled="true"></el-input>
+         </div></el-col>
+       </el-checkbox-group>
+     </el-row>
      <p>Website Address(Require for Internet Merchant)</p>
      <el-input v-model="webSite"></el-input>
    </div>
    <p>Settlement Fee</p>
    <el-input v-model="settlementFee" style="background: #E6E6E6;width: 300px" placeholder="$0.30 Per Batch"></el-input>
+   <p>Settlement Period</p>
+   <el-checkbox-group v-model="checkList">
+     <el-checkbox label="dailyOn">Daily</el-checkbox>
+     <el-checkbox label="every100">Every $100</el-checkbox>
+   </el-checkbox-group>
    <div class="note">
      <el-row :gutter="20">
        <el-col :span="1"><div style="">Note:</div></el-col>
@@ -42,6 +77,7 @@
 export default {
   data () {
     return {
+      checkList: ['unionpayQRCode'],
       serviceFee: '',
       onlineAPI: 'yes',
       needOnlineAPI: true,
@@ -68,6 +104,13 @@ export default {
 
 <style scoped lang="less">
   @import '../assets/css/common.css';
+  .offline-rate{
+    display: inline-flex;
+    .inp{
+      width: 70px;
+      padding-left: 10px;
+    }
+  }
   .note{
     color: #4A4A4A;
     font-family: DINNextLTPro-Regular;
